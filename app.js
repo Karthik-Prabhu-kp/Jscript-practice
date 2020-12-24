6fcc1311655ff853a9d83fd383c8ad9623857210
Karthik-Prabhu-kp <69239406+Karthik-Prabhu-kp@users.noreply.github.com>
2020-12-24 19:18:17 +0530

    feat: check if prime compelete

--- app.js
+++ app.js
@@ -1,11 +1,22 @@
-var btnCheck = document.querySelector("#btn");
-var userInput = document.querySelector("#input-text");
-var divOutput = document.querySelector("#out")
+var btnCheck = document.querySelector("#btn1");
+var divOutput = document.querySelector("#out1")
+
+btnCheck.addEventListener('click' , primeCheckDob) ;
+
+function primeCheckDob(date){
+    
+    var primenumber = parseInt(document.querySelector("#input1").value);
+
+    var result = true;
+    for (var i=2 ; i < date ; i ++){
+      if ( date%i == 0){
+        result= false;
+        break;
+    }}
+    if (result == true){
+        divOutput.innerText = primenumber + " is a prime number!!"
+    }
+    else{divOutput.innerText = primenumber + " is NOT a prime number.."}
+}
 
 
-
-function clickHandler(){
-    divOutput.innerText = userInput.value
-};
-
-btnCheck.addEventListener('click' , clickHandler) 
