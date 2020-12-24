74ae235dfdc3f25d619905b6950e2a412ed7a46d
Karthik-Prabhu-kp <69239406+Karthik-Prabhu-kp@users.noreply.github.com>
2020-12-24 09:04:07 +0530

    alerts and prompt

---
+++ app.js
@@ -0,0 +1,5 @@
+var input=prompt("what is your fav game?", "***" );
+
+if (input.toUpperCase() == "COD" || input.toUpperCase() == "CALL OF DUTY"){
+    alert("Call of duty is my fav game as well!!")
+}
