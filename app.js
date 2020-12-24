755abf384de83994a828132c2dbc5e7e423b6e17
Karthik-Prabhu-kp <69239406+Karthik-Prabhu-kp@users.noreply.github.com>
2020-12-24 19:46:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,20 +3,23 @@ var divOutput = document.querySelector("#out1")
 
 btnCheck.addEventListener('click' , primeCheckDob) ;
 
-function primeCheckDob(date){
+function primeCheckDob(){
     
     var primenumber = parseInt(document.querySelector("#input1").value);
 
     var result = true;
-    for (var i=2 ; i < date ; i ++){
-      if ( date%i == 0){
-        result= false;
+    for (var i=2 ; i < primenumber ; i ++){
+      if ( primenumber % i == 0){
+        result = false;}
         break;
-    }}
+    
+      }
     if (result == true){
         divOutput.innerText = primenumber + " is a prime number!!"
     }
-    else{divOutput.innerText = primenumber + " is NOT a prime number.."}
+    else if (result == false){
+        divOutput.innerText = primenumber + " is NOT a prime number.."
+    }
 }
 
 
